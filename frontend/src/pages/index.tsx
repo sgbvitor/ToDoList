6bf88a91099882f6908986cfbf820fc5bf56ca82
Vitor Gabriel <silva.gbvitor@gmail.com>
2024-06-07 18:02:58 -0300

    fix: update task

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,8 +2,10 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { ChangeEvent, useEffect, useState } from "react";
 import formatDate from "@/utils/formatDate";
+import { Modal, Button, Form } from "react-bootstrap";
 
 export default function Home() {
+  const [newTask, setNewTask] = useState<{ title: string }>({ title: "" });
   const [tasks, setTasks] = useState<
     Array<{ id: number; title: string; status: string; created_at: string }>
   >([]);
@@ -11,6 +13,24 @@ export default function Home() {
     Array<{ id: number; title: string; status: string }>
   >([]);
   const router = useRouter();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  function handleInputChangeCreate(e: React.ChangeEvent<HTMLInputElement>) {
+    setNewTask({ ...newTask, [e.target.name]: e.target.value });
+  }
+
+  function handleCreateNewTask() {
+    axios
+      .post("http://localhost:3020/tasks", { title: newTask.title })
+      .then(() => {
+        router.reload();
+      })
+      .catch((error) => {
+        console.error("Error creating new task", error);
+      });
+  }
 
   function getTasks() {
     axios
@@ -62,7 +82,6 @@ export default function Home() {
         });
     }
   }
-  console.log(updatedTask);
   return (
     <main className="container p-5">
       <table className="table table-bordered table-dark table-hover mx-auto align-middle">
@@ -71,8 +90,11 @@ export default function Home() {
             <th scope="col">#</th>
             <th scope="col">Title</th>
             <th scope="col">Status</th>
-            <th scope="col" colSpan={2}>
-              Created_at
+            <th scope="col">Created_at</th>
+            <th className="text-center" scope="col">
+              <Button className="btn btn-success mx-1" onClick={handleShow}>
+                <i className="bi bi-plus-square"></i>
+              </Button>
             </th>
           </tr>
         </thead>
@@ -83,7 +105,7 @@ export default function Home() {
               <td className="d-flex flex-direction-row flex-wrap">
                 <textarea
                   placeholder={task.title}
-                  className="form-control bg-transparent border-0 outline-0 p-0 flex-grow-1"
+                  className="form-control bg-transparent border-0 outline-0 p-0 flex-grow-1 overflow-auto"
                   onBlur={() => handleUpdateTask(task.id)}
                   name="title"
                   onChange={handleInputChange}
@@ -108,9 +130,6 @@ export default function Home() {
               <td>{formatDate(task.created_at)}</td>
               <td>
                 <div className="text-center d-flex gap-2 flex-wrap align-items-center justify-content-center">
-                  <button className="btn btn-success mx-1">
-                    <i className="bi bi-plus-square"></i>
-                  </button>
                   <button
                     onClick={() => clickDelete(task.id)}
                     className="btn btn-danger mx-1"
@@ -123,6 +142,39 @@ export default function Home() {
           ))}
         </tbody>
       </table>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header
+          closeButton
+          className="bg-dark text-white border-bottom-0"
+        >
+          <Modal.Title>New Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="bg-dark text-white">
+          <Form className="bg-dark text-white">
+            <Form.Group
+              className="mb-3 bg-dark text-white"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                name="title"
+                className="mb-3 bg-secondary text-white"
+                as="textarea"
+                rows={3}
+                onChange={handleInputChangeCreate}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer className="bg-dark text-white border-top-0">
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handleCreateNewTask}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </main>
   );
 }
